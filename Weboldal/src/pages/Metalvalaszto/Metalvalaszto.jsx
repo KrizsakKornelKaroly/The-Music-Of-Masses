99503d67ebe277f512964ec14874111496c6ddad
MrErik402 <bujakierikattila@turr.hu>
2024-06-04 19:10:06 +0200

    TMOM-53 - Metal alstílus választó lefejlesztésének megkezdése. V1.0

--- Weboldal/src/pages/Metalvalaszto/Metalvalaszto.jsx
+++ Weboldal/src/pages/Metalvalaszto/Metalvalaszto.jsx
@@ -1,10 +1,32 @@
-import Navbar from "../../components/Navbar"
+import Navbar from "../../components/Navbar";
+import "./Metalvalaszto.css";
+//import { IoMdArrowDropright } from "react-icons/io";
 
 const Metalvalaszto = () => {
     return (
       <>
       <Navbar />
-        
+      <main>
+            <div className="cimSzoveg">
+              <h1 id="cimM">Metal stílus</h1>
+              <p id="szovegM">Kemény erős nőket szeretem. Amíg le tudom győzni őket szaknderben, nincs gond.</p>
+              <span id="alkotoM">James Hetfield</span>
+            </div>
+            <img src="./public/JamesHetfield.png" id="kep"/>
+            <div>
+              <h1 id="cimMa">Alstílus</h1>
+              <img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+            </div>
+            
+            <div className="ZenekarElrendezes">
+              <h1 className="h1ZE">Külföldi Zenekarok</h1>
+              <h1 className="h1ZE">Belföldi Zenekarok</h1>
+              <div className="zenekarok"><span>asd</span></div>
+              <div className="zenekarok">sad</div>
+              <div className="zenekarok">asd</div>
+              <div className="zenekarok">sad</div>
+            </div>
+      </main>
 
       </>
     )
